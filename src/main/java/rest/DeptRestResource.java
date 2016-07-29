b24ca849f8f1ea8fe9e80a75cfb89aa645eb47f0
Pablo Curty <pablocurty@id.uff.br>
2016-07-29 15:26:06 -0300

    rest funcionando com servidor tomcat

--- src/main/java/rest/DeptRestResource.java
+++ src/main/java/rest/DeptRestResource.java
@@ -1,6 +1,5 @@
 package rest;
 
-import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,6 +7,10 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import app.model.Dept;
 import app.service.DeptAppService;
 import excecao.DepartamentoNaoEncontradoException;
 
@@ -16,15 +19,17 @@ import excecao.DepartamentoNaoEncontradoException;
 @Consumes("application/json;charset=utf-8")
 public class DeptRestResource {
 
-	@Inject
-	DeptAppService deptAppService;
+	ApplicationContext fabrica = new ClassPathXmlApplicationContext("beans-jpa.xml");
+	
+	DeptAppService deptAppService = (DeptAppService) fabrica.getBean("deptAppService");
 	
 	@GET
 	@Path("buscaID/{id}")
 	public String getNomeDept(@PathParam("id") Long id){
 		String name = "dept não encontrado";
 		try {
-			name = deptAppService.getOneDept(id).getNameDept();
+			Dept dept = deptAppService.getOneDept(id);
+			name = dept.getNameDept();
 		} catch (DepartamentoNaoEncontradoException e) {
 			e.printStackTrace();
 		}
@@ -32,7 +37,6 @@ public class DeptRestResource {
 	}
 	
 	@GET
-	@Path("hello")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String hello(){
 		
